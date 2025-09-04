a80bcd0aaf623d0d0afb3fc2e8d5560e78dd4dcd
Willian Eliseu da Silva <w.eliseu@gmail.com>
2025-09-04 12:27:18 -0300

    project completed. All done.

--- src/js/script.js
+++ src/js/script.js
@@ -21,4 +21,13 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     });
   });
+
+  const navbar = document.getElementById('navbar');
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > 70) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
 });
